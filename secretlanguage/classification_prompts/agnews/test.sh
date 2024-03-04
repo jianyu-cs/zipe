eae5e619b96ccf92b379a582d2e189da73f69201
Jianyu <jiw102@ucsd.edu>
2024-03-03 23:21:23 -0800

    a

--- secretlanguage/classification_prompts/agnews/test.sh
+++
@@ -1,4 +0,0 @@
-python converting.py --file="natural_prompts"
-python converting.py --file="gibberish_prompts"
-python converting.py --file="gibberish_prompts_masked"
-python converting.py --file="natural_prompts_masked"
